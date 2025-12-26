324ee4b5d580f8f8809302c2275175866ee5efb4
sutigit <panyanilben@gmail.com>
2025-12-26 17:02:52 +0200

    add tabs component

--- src/components/SatDetail.tsx
+++ src/components/SatDetail.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "preact/hooks"
+import { useEffect, useRef, useState } from "preact/hooks"
 import { cesiumView } from "../cesium/renderer"
 import { addObserverGround, addSatellitePointGround, addSatellitePathGround } from "../cesium/add"
 import { useSatellites } from "../context/ContextAPI"
 import type { Satellite } from "../types"
 import { Measure } from "./common/Measure"
+import { TabBody, TabHeader } from "./common/Tabs"
 
 export default function SatDetail() {
     const cesiumMinimapRef = useRef(null)
@@ -45,31 +46,19 @@ export default function SatDetail() {
 
 const SatelliteDetails = ({ satellite }: { satellite: Satellite | undefined }) => {
     if (!satellite) return
+    const [tab, setTab] = useState<number>(0)
+    const tabs = ['LIVE', 'L1', 'L2', 'L3', 'L4', 'L5']
 
     return (
-        <div className="" >
+        <div>
             <h3 style={{ padding: '1.5rem' }}>{satellite.name}</h3>
-            <Tabs tab={0} />
-            <div style={{ padding: '2rem', display: 'flex', justifyContent: 'space-between', gap: 5, flexWrap: 'wrap' }}>
+            <TabHeader tab={tab} setTab={setTab} tabs={tabs} />
+            <TabBody tab={tab} tabs={tabs}>
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
-            </div>
-        </div>
-    )
-}
-
-const Tabs = ({ tab }: { tab: number }) => {
-    return (
-        <div style={{ display: 'flex', position: 'relative' }}>
-            <div style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'pink', height: '0.6rem' }} />
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'pink' }}>Live</div>
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP1</div>
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP2</div>
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP3</div>
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP4</div>
-            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP5</div>
+            </TabBody>
         </div>
     )
 }
---
+++ src/components/common/Tabs.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "preact/compat"
+
+export const TabHeader = ({ tab, setTab, tabs }: { tab: number, setTab: (tab: number) => void, tabs: string[] }) => {
+    return (
+        <div className="tab-header">
+            {tabs.map((t, i) => (
+                <div key={t} className={`tab-button ${tab === i ? 'selected' : ''}`} onClick={() => setTab(i)}>{t}</div>
+            ))}
+            <div className="background-piece" />
+        </div>
+    )
+}
+
+export const TabBody = ({ tab, tabs, children, }: { tab: number, tabs: string[], children: ReactNode }) => {
+    return (
+        <div className={`tab-body ${tab === 0 ? 'round-right' : tab === tabs.length - 1 ? 'round-left' : ''}`}>
+            {children}
+        </div>
+    )
+}
