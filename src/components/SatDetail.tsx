7531e118bd02fed1b11108cdfc176a38196ff307
sutigit <panyanilben@gmail.com>
2025-12-26 17:21:13 +0200

    update

--- src/components/SatDetail.tsx
+++ src/components/SatDetail.tsx
@@ -36,7 +36,6 @@ export default function SatDetail() {
                     <div id="cesium-minimap-container">
                         <div id="cesium-minimap-view" ref={cesiumMinimapRef} style={{ position: 'absolute', inset: '0 0 0 0' }} />
                     </div>
-
                     <SatelliteDetails satellite={targetSatellite} />
                 </div>
             </div>
@@ -48,17 +47,39 @@ const SatelliteDetails = ({ satellite }: { satellite: Satellite | undefined }) =
     if (!satellite) return
     const [tab, setTab] = useState<number>(0)
     const tabs = ['LIVE', 'L1', 'L2', 'L3', 'L4', 'L5']
+    const renderTabPage = (tab: number) => (tab === 0 ? <LiveSatelliteDetail /> : <LookUpSpotDetail spot={tab} />)
 
     return (
         <div>
             <h3 style={{ padding: '1.5rem' }}>{satellite.name}</h3>
             <TabHeader tab={tab} setTab={setTab} tabs={tabs} />
             <TabBody tab={tab} tabs={tabs}>
-                <Measure label="Speed" value={0.0} unit="km/s" />
-                <Measure label="Speed" value={0.0} unit="km/s" />
-                <Measure label="Speed" value={0.0} unit="km/s" />
-                <Measure label="Speed" value={0.0} unit="km/s" />
+                {renderTabPage(tab)}
             </TabBody>
         </div>
     )
 }
+
+const LiveSatelliteDetail = () => (
+    <div className="tab-body-page">
+        <h4 className="title">Live details</h4>
+        <div className="content">
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+        </div>
+    </div>
+)
+
+const LookUpSpotDetail = ({ spot }: { spot: number }) => (
+    <div className="tab-body-page">
+        <h4 className="title">Look up {spot}</h4>
+        <div className="content">
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+            <Measure label="Speed" value={0.0} unit="km/s" />
+        </div>
+    </div>
+)
