3fa79e40cee4e3e7bb5184d8dd1c9e228c883d26
sutigit <panyanilben@gmail.com>
2025-12-26 13:41:13 +0200

    update colors

--- src/components/SatDetail.tsx
+++ src/components/SatDetail.tsx
@@ -47,14 +47,29 @@ const SatelliteDetails = ({ satellite }: { satellite: Satellite | undefined }) =
     if (!satellite) return
 
     return (
-        <div className="" style={{ padding: '1.5rem' }}>
-            <h3>{satellite.name}</h3>
-            <div style={{ marginTop: '2rem', display: 'flex', justifyContent: 'space-between', gap: 5, flexWrap: 'wrap' }}>
+        <div className="" >
+            <h3 style={{ padding: '1.5rem' }}>{satellite.name}</h3>
+            <Tabs tab={0} />
+            <div style={{ padding: '2rem', display: 'flex', justifyContent: 'space-between', gap: 5, flexWrap: 'wrap' }}>
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
                 <Measure label="Speed" value={0.0} unit="km/s" />
             </div>
+        </div>
+    )
+}
+
+const Tabs = ({ tab }: { tab: number }) => {
+    return (
+        <div style={{ display: 'flex', position: 'relative' }}>
+            <div style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'pink', height: '0.6rem' }} />
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'pink' }}>Live</div>
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP1</div>
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP2</div>
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP3</div>
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP4</div>
+            <div style={{ zIndex: 2, flex: 1, borderRadius: '0.8rem', display: 'flex', justifyContent: 'center', padding: '0.6rem 0', backgroundColor: 'coral' }}>LP5</div>
         </div>
     )
 }
